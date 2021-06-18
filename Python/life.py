b11104af55c79f2594682f8542fee0e7a50eb011
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2021-06-18 16:56:02 +0530

    Add files via upload

---
+++ Python/life.py
@@ -0,0 +1,10 @@
+theAnswer = 42
+
+
+def quote_marvin():
+    print('I\'ve Calculated the chance of your Survival. But I dont think you\'ll like it. ')
+
+import math 
+
+def square_root(num):
+    return math.sqrt(num)
